6af10d0130c1bc30d9cd4d2ffe2d3d3ca64eb09e
Rutvik Patel <rutvik.p4t3l@gmail.com>
2025-06-24 02:25:19 -0700

    refactor: use lodash to setup templates and compile format strings, and use data files for loading string assets

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,17 +1,31 @@
 class Footer extends HTMLElement {
     async connectedCallback() {
-        const html = await window.templateLoader.loadTemplate('static/html/footer.html');
-        if (html) {
-            const name = this.getAttribute('name') || '';
-            const tagline = this.getAttribute('tagline') || '';
-            const year = new Date().getFullYear();
+        const htmlTemplate = await window.templateLoader.loadTemplate('static/html/footer.html');
 
-            const rendered = html
-                .replace('{{year}}', year)
-                .replace('{{name}}', name)
-                .replace('{{tagline}}', tagline);
+        if (htmlTemplate) {
+            try {
+                const response = await fetch('static/assets/data.json');
+                const rawData = await response.json();
 
-            this.innerHTML = rendered;
+                const dataForTemplate = {
+                    year: new Date().getFullYear(),
+                    name: _.get(rawData, ['footer', 'name'], ''),
+                    tagline: _.get(rawData, ['footer', 'tagline'], '')
+                };
+
+                const compiledTemplate = _.template(htmlTemplate);
+                this.innerHTML = compiledTemplate(dataForTemplate);
+                console.log(compiledTemplate(dataForTemplate));
+
+            } catch (error) {
+                console.error('Error loading or processing footer data:', error);
+                const compiledTemplate = _.template(htmlTemplate);
+                this.innerHTML = compiledTemplate({
+                    year: '',
+                    name: '',
+                    tagline: ''
+                });
+            }
         }
     }
 }
